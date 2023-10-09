243de2a78804d3232e5afdf0d40585e0e3a1081a
chenchaiah <rchenchaiah98@gmail.com>
2023-10-09 17:11:30 +0000

    files added

---
+++ src/components/GithubPopularRepos/index.js
@@ -0,0 +1,9 @@
+const languageFiltersData = [
+  {id: 'ALL', language: 'All'},
+  {id: 'JAVASCRIPT', language: 'Javascript'},
+  {id: 'RUBY', language: 'Ruby'},
+  {id: 'JAVA', language: 'Java'},
+  {id: 'CSS', language: 'CSS'},
+]
+
+// Write your code here
